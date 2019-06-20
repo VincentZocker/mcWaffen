8fbecd67afd8f4acfe8ceb0b672ec9bd6b82eaf8
vincent <vincentzocker@gmail.com>
2019-06-20 17:46:52 +0200

    Feuerschwert Listener

--- FeuerschwertPlugin.java
+++ FeuerschwertPlugin.java
@@ -6,6 +6,22 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FeuerschwertPlugin extends JavaPlugin {
+
+    public class FeuerschwertListener implements Listener {
+        @EventHandler
+        public void onEvent(PlayerInteractEvent event){
+            ItemStack gegenstand = event.getItem();
+            ItemMeta metaData = gegenstand.getItemMeta();
+
+            if(metaData.getDisplayName().equals("Feuerschwert")){
+                Block block = event.getClickedBlock();
+                Block blockh = block.getRelative(BlockFace.UP);
+                blockh.setType(Material.FIRE);
+                event.setCancelled(true);
+            }
+        }
+    }
+
     public void onEnable() {
         ItemStack feuerschwert = new ItemStack(Material.IRON_SWORD);
 	ItemMeta metaData = feuerschwert.getItemMeta();
@@ -17,6 +33,10 @@ public class FeuerschwertPlugin extends JavaPlugin {
         feuerschwertRezept.addIngredient(Material.IRON_SWORD);
         feuerschwertRezept.addIngredient(Material.FLINT_AND_STEEL);
         this.getServer().addRecipe(feuerschwertRezept);
+
+        PluginMananger pluginMananger = this.getServer().getPluginManager();
+        FeuerschwertListener listener = new FeuerschwertListener();
+        pluginManager.registerEvents(listener, this);
     }
 
     public void onDisable() {
